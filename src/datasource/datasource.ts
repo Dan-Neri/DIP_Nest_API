97d30559bbfbee0c942a1345244d24f427b33150
Dan Neri <35468623+Dan-Neri@users.noreply.github.com>
2024-06-27 16:18:08 -0700

    Add database configuration and todos module

---
+++ src/datasource/datasource.ts
@@ -0,0 +1,29 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  logging: true,
+  synchronize: false,
+  ssl: process.env.ENVIRONMENT === 'production' ? true : false,
+  extra:
+    process.env.ENVIRONMENT === 'production'
+      ? {
+          ssl: {
+            rejectUnauthorized:
+              process.env.ENVIRONMENT === 'production' ? false : true,
+          },
+        }
+      : null,
+  entities: [`${__dirname}/../**/**.entity{.ts,.js}`],
+  migrations: [`${__dirname}/../migrations/*{.ts,.js}`],
+});
+
+export default dataSource;
